e0df4a8324361297dc901658c798ab85ad175838
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2020-12-30 17:39:40 -0300

    Cherry-pick improvements from beer

--- example/uphandler.py
+++ example/uphandler.py
@@ -1,4 +1,5 @@
-from django.core.files.uploadhandler import TemporaryFileUploadHandler, MemoryFileUploadHandler
+from django.conf import settings
+from django.core.files.uploadhandler import TemporaryFileUploadHandler, MemoryFileUploadHandler, StopUpload
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
@@ -9,9 +10,9 @@ COOKIE_KEY = 'djangochannel'
 class ChannelFileUploadHandler:
     def __init__(self, request=None):
         super().__init__(request)
-        if COOKIE_KEY in request.COOKIES:
+        try:
             self.channel_name = request.COOKIES[COOKIE_KEY]
-        else:
+        except KeyError:
             self.channel_name = None
 
     def send_consumer(self, method, *args):
@@ -39,6 +40,8 @@ class ChannelTemporaryFileUploadHandler(ChannelFileUploadHandler, TemporaryFileU
     def receive_data_chunk(self, raw_data, start):
         super().receive_data_chunk(raw_data, start)
         self.partial = min(self.partial + self.chunk_size, self.total)
+        if self.partial > settings.FILE_UPLOAD_MAX_TEMP_SIZE:
+            raise StopUpload()
         progress = int(100 * (self.partial / self.total) + 0.5)
         if progress > self.progress:
             self.progress = progress
